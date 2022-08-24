5b933b3bbe8791a3473632b838105f13038e1da0
bgadd_brad <bradleygad@gmail.com>
2022-08-24 18:25:45 -0400

    adding health scriptable object

---
+++ Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "stats/health" , fileName = "New Health")]
+public class Health : ScriptableObject
+{
+}
